a8d2c6ecc9c828d77c0322a4751ff52b70d4ab7a
khanhtuanvo <tuankhanhdn8@gmail.com>
2025-04-30 01:39:56 -0400

    Add UI to the project

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 function getComputerChoice(){
-    computerChoice = Math.random()*4;
-    if (computerChoice <= 1){
-        return 'Rock';
-    }
-    else if (computerChoice <= 2){
-        return 'Paper';
-    }
-    else{
-        return 'Scissors';
-    }
+    const choices = ['Rock','Paper','Scissors'];
+    const computerChoice = Math.floor(Math.random()*3);
+    return choices[computerChoice];
 }
 
 function getUserChoice(){
@@ -16,33 +9,66 @@ function getUserChoice(){
     return userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLowerCase();
 }
 function playRound(userChoice, computerChoice){
+    let result = '';
     if (userChoice == computerChoice){
-        console.log("It's a tie!");
+        result = "It's a tie!";
     }
     else if (userChoice == 'Rock' && computerChoice == 'Scissors' || userChoice == 'Paper' && computerChoice == 'Rock' || userChoice == 'Scissors' && computerChoice == 'Paper'){
-        console.log(`You win! ${userChoice} beats ${computerChoice}`);
+        result = `You win! ${userChoice} beats ${computerChoice}`;
         userScore += 1;
+        userScoreSpan.textContent = userScore;
     }
     else{
-        console.log(`You lose! ${computerChoice} beats ${userChoice}`);
+        result = `You lose! ${computerChoice} beats ${userChoice}`;
         computerScore += 1;
+        computerScoreSpan.textContent = computerScore;
+    }
+    resultDiv.innerHTML = `
+        <p>You choose ${userChoice}</p>
+        <p>Computer choose ${computerChoice}</p>
+        <p>${result}</p>
+        `;
+    if (userScore == 5){
+        resultDiv.innerHTML = "You win the game";
+    }
+    else if (computerScore == 5){
+        resultDiv.innerHTML = "Computer win the game";
     }
 }
 let userScore = 0;
 let computerScore = 0;
+const resultDiv = document.getElementById("result");
+const userScoreSpan = document.getElementById("user-score");
+const computerScoreSpan = document.getElementById("computer-score");
 
-function playGame(){
-    console.log("Welcome to rock paper scissors game");
-    do{
-        const humanSelection = getUserChoice();
-        const computerSelection = getComputerChoice();
-        console.log(`You choose ${humanSelection}`);
-        console.log(`Computer choose ${computerSelection}`);
-        playRound(humanSelection, computerSelection);
-        console.log(`Your score is ${userScore}`);
-        console.log(`Computer score is ${computerScore}`);
-        var continueChoice = prompt("Wanna continue !! (Enter 'yes' to continue ,'no' to exit: ")
-    } while (continueChoice == 'yes');
-    console.log('Goodbye! Have a great day!');
-}
-playGame();
+document.getElementById('rock').addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    playRound('Rock', computerSelection);
+});
+
+document.getElementById('paper').addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    playRound('Paper', computerSelection);
+});
+
+document.getElementById('scissors').addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    playRound('Scissors', computerSelection);
+});
+
+// function playGame(){
+//     console.log("Welcome to rock paper scissors game");
+//     do{
+//         const humanSelection = getUserChoice();
+//         const computerSelection = getComputerChoice();
+//         result = `You choose ${humanSelection}`;
+//         console.log(`Computer choose ${computerSelection}`);
+//         playRound(humanSelection, computerSelection);
+//         result = `Your score is ${userScore}`;
+//         console.log(`Computer score is ${computerScore}`);
+//         var continueChoice = prompt("Wanna continue !! (Enter 'yes' to continue ,'no' to exit: ")
+//     } while (continueChoice == 'yes');
+//     console.log('Goodbye! Have a great day!');
+// }
+// playGame();
+
